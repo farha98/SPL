64edc7422a03a9f928e3885436c9a994b2bac8d4
Farha Tasnim <farhatasnim403@gmail.com>
2026-01-18 20:01:16 +0600

    Create j.c

---
+++ sheet 1(Data type)/j.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+ 
+int main()
+{
+    int a;
+    int b;
+    scanf("%d %d", &a, &b);
+ 
+    if (b% a == 0 || a% b == 0)
+        printf("Multiples");
+    else
+        printf("No Multiples");
+ 
+    return 0;
+ 
+    
+}
